8cf9e0b604fb2355c5507411b9a6e7cdef5f5187
Mohammed Rahman <mohammedrahman@Mohammeds-MacBook-Pro.local>
2022-08-26 11:22:03 -0400

    job app done

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,88 +1,70 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
+ /*
+    Note to reader: this was made quickly, for job application for Estee Lauder
+    -Author : Mohammed Rahman
+            - this is the test page  that contains 2 tests.
+            a. first validates URL
+            b. goes to a specific item, and validates add to cart functionality
+            - note : a lot of the links take extra time to load, perhaps this is a work in progress
+            - also i did not create any other classes like the Base class as this was only for
+            demonstrative purposes. I can certainly continue to build using this Page Object Model
+*/
 
-public class Test1 {
-    WebDriver driver;
-    public Object Utility;
 
-    @BeforeClass
-    public void testSetup() {
-        System.setProperty("webdriver.chrome.driver", "/Users/mohammedrahman/Downloads/chromedriver 3");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.manage().deleteAllCookies();
-        // Dimension class with browser width and height value passed
-        Dimension dem = new Dimension(1350, 850);
-        // passing the Dimension object as an argument to setSize method
-        driver.manage().window().setSize(dem);
+public class Test1 extends Utility {
+
+        @Test(priority = 0)
+            public void validateURLv2(){
+
+            String baseUrl = "https://www.esteelauder.com/products/19239/product-catalog/last-chance";
+            String expectedTitle = "Last Chance Beauty Products | Estée Lauder";
+            String actualTitle = "";
+            driver.get(baseUrl);
+
+            // get the actual value of the title
+            actualTitle = driver.getTitle();
+            System.out.println(actualTitle);
+            Assert.assertEquals(actualTitle,expectedTitle);
     }
-//    @Test
-    //This test works
-//            public void validateURLv2(){
-//
-//            String baseUrl = "https://www.esteelauder.com/products/19239/product-catalog/last-chance";
-//            String expectedTitle = "Last Chance Beauty Products | Estée Lauder";
-//            String actualTitle = "";
-//            driver.get(baseUrl);
-//
-//            // get the actual value of the title
-//            actualTitle = driver.getTitle();
-//            System.out.println(actualTitle);
-//
-//            /*
-//             * compare the actual title of the page with the expected one and print
-//             * the result as "Passed" or "Failed"
-//             */
-//            if (actualTitle.contentEquals(expectedTitle)){
-//                System.out.println("Test Passed!");
-//            } else {
-//                System.out.println("Test Failed");
-//            }
-//            Assert.assertEquals(actualTitle,expectedTitle);
-//    }
-    @Test
-    public void validateAddtoCart() throws AWTException, InterruptedException {
-        String baseUrl = "https://www.esteelauder.com/product/26389/2650/product-catalog/skinca" +
-               "re/toner-treatment-lotion/perfectly-clean/fresh-balancing-lotion?size=13.5_oz.";
+    @Test(priority = 1)
+    public void validateAddtoCart() {
+        String baseUrl = "https://www.esteelauder.com/product/684/25713/product-catalog/skincare/cleanser-makeup-remover/perfectly-clean/multi-action-foam-cleanserpurifying-mask?size=5.0_oz.";
         driver.get(baseUrl);
-        Thread.sleep(3000);
-        driver.findElement(By.xpath("//button[@class='gnav-signup-" +
-                "overlay__close js-gnav-signup-overlay-close']")).click();
-//        Robot robot = new Robot();
-//        robot.mouseMove(1228, 227); // move mouse point to specific location
-////        robot.delay(1500);        // delay is to make code wait for mentioned milliseconds before executing next step
-//        // first click
-//        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-//        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-//        // second click
-//        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-//        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-//        robot.delay(1500);
-//        // not working so itried a different method
+        holdYourHorses(5);
+        try{
+            driver.findElement(By.xpath("//button[@class='gnav-signup-" +
+                    "overlay__close js-gnav-signup-overlay-close']")).click();
+        }catch (Exception NoSuchElementException){
+            System.out.println("Detected no pop up elem");
+        }
+        //get cost of item
+        String itemCost = driver.findElement(By.xpath("(//h4[contains(.,'$')])[1]")).getText();
+        itemCost = itemCost.replace("$","");
+        Double itemCostDouble = Double.parseDouble(itemCost);
 
-//        Utility = new Utility("//button[@title='Add To Bag']");
-//        ScrollDownUp();
+        String itemname = driver.findElement(By.xpath("//div[@class='elc-right-column-section js-right-column-section']//following-sibling::h4")).getText();
         driver.findElement(By.xpath("//button[@title='Add To Bag']")).click();
-
-
-        Thread.sleep(5000);
+        holdYourHorses(5);
         driver.findElement(By.xpath(("//span[@class='utility-nav__cart-icon']"))).click();
-    }
 
-    private void ScrollDownUp() {
-        Actions actions = new Actions(driver);
-        actions.sendKeys(Keys.PAGE_DOWN).build().perform();
-        actions.sendKeys(Keys.PAGE_UP).build().perform();
+        WebElement element = driver.findElement(By.xpath("//a[@class='cart-item__product-subname--text']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+        holdYourHorses(5);
+
+        //verify cart contain item and cost of item
+        String cartItemName= element.getText().toLowerCase(Locale.ROOT);
+        Assert.assertTrue(cartItemName.contains(itemname.toLowerCase(Locale.ROOT)));
     }
 
     @AfterMethod
@@ -96,9 +78,3 @@ public class Test1 {
         driver.quit();
     }
 }
-
-
-
-
-
-
--- src/main/java/Utility.java
+++ src/main/java/Utility.java
@@ -1,27 +1,55 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeClass;
 
-public class Utility extends Test1{
+import java.util.concurrent.TimeUnit;
 
-    public Utility(String s) {
+ /*
+    Note to reader: this was made quickly, for job application for Estee Lauder
+    -Author : Mohammed Rahman
+            - the utlities here are some of the tools i used to maneuvar the website
+            - note : a lot of the links take extra time to load, perhaps this is a work in progress
+            - there are popup links appearing constantly on the webpage, which delays automation
+            i created different methods to bypass such concerns, but in the end a
+             try catch worked the best, where i catch the link and use .click to close the elmenet
+*/
 
-        super();
+public class Utility {
+    WebDriver driver;
+    public Object Utility;
+
+
+
+    @BeforeClass
+    public void testSetup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().deleteAllCookies();
+        Dimension dem = new Dimension(1350, 850);
+        driver.manage().window().setSize(dem);
     }
 
-    public void Actionsclass(String e){
+
+    public void tryHover(String e){
         Actions actions = new Actions(driver);
-
-        //Retrieve WebElement 'Music' to perform mouse hover
         WebElement menuOption = driver.findElement(By.xpath(e));
-        //Mouse hover menuOption 'Music'
         actions.moveToElement(menuOption).perform();
-        System.out.println("Done Mouse hover on 'Music' from Menu");
-
+//        System.out.println("Done Mouse hover on 'Music' from Menu");
     }
+
     public void SrollDown(){
         Actions actions = new Actions(driver);
         actions.sendKeys(Keys.PAGE_DOWN).build().perform();
+    }
+
+    public static void holdYourHorses(int seconds){
+        try{
+            Thread.sleep(seconds*1000);
+        }catch (Exception InterruptedException){
+            System.out.println("Thread exception");
+        }
     }
 }
